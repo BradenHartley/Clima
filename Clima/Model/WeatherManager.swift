cff3cf0ff4e94917d5cd9af6ddeef0dd494a161e
Braden Hartley <Bradenhartley@hotmail.com>
2023-03-07 21:56:54 -0700

    first commit

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Braden Hartley on 2/3/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/Model/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Braden Hartley on 2/4/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
